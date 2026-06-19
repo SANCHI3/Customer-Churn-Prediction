c7ca660740228309102da6c3dd151d755435335e
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-06-19 13:09:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 import streamlit as st
 import joblib
 
-st.set_page_config(
-    page_title="Customer Churn Predictor",
-    page_icon="📊",
-    layout="wide"
-)
+# Load pipeline
+pipeline = joblib.load("churn_pipeline.pkl")
+model = pipeline["model"]
+
+st.set_page_config(page_title="Customer Churn Predictor")
 
 st.title("📊 Customer Churn Prediction System")
 st.write("Predict telecom customer churn risk using Machine Learning")
 
-pipeline = joblib.load("churn_pipeline.pkl")
+# User Inputs
+tenure = st.number_input("Tenure (Months)", min_value=0, max_value=72, value=12)
 
-st.success("Model Loaded Successfully!")
+monthly_charges = st.number_input(
+    "Monthly Charges",
+    min_value=0.0,
+    max_value=200.0,
+    value=70.0
+)
+
+contract = st.selectbox(
+    "Contract Type",
+    ["Month-to-month", "One year", "Two year"]
+)
+
+if st.button("Predict Churn"):
+    st.success("Button Click Working!")
