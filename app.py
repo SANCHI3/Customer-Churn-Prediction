52a8275f3535799c5b0503dceeef49f1e16f6ac4
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-06-19 13:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,95 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 import joblib
 
-# Load pipeline
 pipeline = joblib.load("churn_pipeline.pkl")
+
 model = pipeline["model"]
+scaler = pipeline["scaler"]
+feature_names = pipeline["feature_names"]
 
 st.set_page_config(page_title="Customer Churn Predictor")
 
 st.title("📊 Customer Churn Prediction System")
 st.write("Predict telecom customer churn risk using Machine Learning")
-st.write("VERSION 2")
-# User Inputs
-tenure = st.number_input("Tenure (Months)", min_value=0, max_value=72, value=12)
 
-monthly_charges = st.number_input(
+st.header("Customer Information")
+
+gender = st.selectbox("Gender", ["Female", "Male"])
+senior = st.selectbox("Senior Citizen", [0, 1])
+partner = st.selectbox("Partner", ["No", "Yes"])
+dependents = st.selectbox("Dependents", ["No", "Yes"])
+
+tenure = st.number_input("Tenure", 0, 72, 12)
+
+monthly = st.number_input(
     "Monthly Charges",
     min_value=0.0,
     max_value=200.0,
     value=70.0
 )
 
+total = st.number_input(
+    "Total Charges",
+    min_value=0.0,
+    value=500.0
+)
+
+phone = st.selectbox("Phone Service", ["No", "Yes"])
+paperless = st.selectbox("Paperless Billing", ["No", "Yes"])
+
+multiple = st.selectbox(
+    "Multiple Lines",
+    ["No", "Yes", "No phone service"]
+)
+
+internet = st.selectbox(
+    "Internet Service",
+    ["DSL", "Fiber optic", "No"]
+)
+
+online_security = st.selectbox(
+    "Online Security",
+    ["No", "Yes", "No internet service"]
+)
+
+online_backup = st.selectbox(
+    "Online Backup",
+    ["No", "Yes", "No internet service"]
+)
+
+device_protection = st.selectbox(
+    "Device Protection",
+    ["No", "Yes", "No internet service"]
+)
+
+tech_support = st.selectbox(
+    "Tech Support",
+    ["No", "Yes", "No internet service"]
+)
+
+streaming_tv = st.selectbox(
+    "Streaming TV",
+    ["No", "Yes", "No internet service"]
+)
+
+streaming_movies = st.selectbox(
+    "Streaming Movies",
+    ["No", "Yes", "No internet service"]
+)
+
 contract = st.selectbox(
-    "Contract Type",
+    "Contract",
     ["Month-to-month", "One year", "Two year"]
 )
 
-if st.button("Predict Churn"):
-    st.success("Button Click Working!")
+payment = st.selectbox(
+    "Payment Method",
+    [
+        "Bank transfer (automatic)",
+        "Credit card (automatic)",
+        "Electronic check",
+        "Mailed check"
+    ]
+)
